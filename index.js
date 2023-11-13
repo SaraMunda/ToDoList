011578ca4e7dab876af122fa37d06bab2a6e7449
Sara <saramunda99@gmail.com>
2023-11-13 17:11:05 +0100

    I created <li> elements, checkbox elements and buttons close with Javascript and added the style with CSS

---
+++ index.js
@@ -0,0 +1,30 @@
+let input = document.getElementById("input-el")
+let ulEl= document.getElementById("ul-el")
+let addBtn = document.getElementById("addBtn")
+
+
+addBtn.addEventListener("click",()=>{
+    let list= input.value
+
+    if(list){
+        const li = document.createElement("li")
+        li.className = "list-group-items"
+        li.textContent = list
+        ulEl.appendChild(li)
+
+        const checkbox = document.createElement("input")
+        checkbox.type = "checkbox"
+        checkbox.className = "form-check-input"
+        li.appendChild(checkbox)
+
+        const button = document.createElement ("input")
+        button.type ="button"
+        button.className = "btn-close"
+        button.addEventListener("click", function(){
+            li.remove()
+        })
+        li.appendChild(button)
+
+        input.value=""
+    }
+})
