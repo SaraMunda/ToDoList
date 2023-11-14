6de5edf7d6ec5a05c39f684663259ae2fac6a0a9
Sara <saramunda99@gmail.com>
2023-11-14 17:23:02 +0100

    I created the clear notes and the clear checked buttons, and an overflow menu

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let input = document.getElementById("input-el")
 let ulEl = document.getElementById("ul-el")
 let addBtn = document.getElementById("addBtn")
 let clearBtn = document.getElementById("clearBtn")
+let notes = document.getElementById("notes")
+let notesBtn = document.getElementById("clearNotesBtn")
+let clearCheckedBtn = document.getElementById("clearChecked")
 
 
 addBtn.addEventListener("click",()=>{
@@ -21,19 +24,65 @@ addBtn.addEventListener("click",()=>{
         const button = document.createElement ("input")
         button.type ="button"
         button.className = "btn-close"
+        //Remove the single items
         button.addEventListener("click", function(){
             li.remove()
         })
+        
         li.appendChild(button)
 
         input.value=""
     }
 })
 
-function clearItems(){
-    ulEl.innerHTML= ""
+//Clear all the items
+
+function clearAllItems(){
+    ulEl.innerHTML=""
 }
 
 clearBtn.addEventListener("click",()=>{
-    clearItems()
+    const listItems = ulEl.getElementsByTagName("li")
+
+    if (listItems.length > 0) {
+        const confirmation = window.confirm("Are you sure you want to delete all the items?")
+        if (confirmation) {
+            clearAllItems()
+        }
+    } else {
+        const confirmation = window.confirm("There are no items to delete")
+    }
+})
+
+//Clear all the notes
+
+function clearNotes(){
+    notes.value=""
+}
+
+notesBtn.addEventListener("click",()=>{
+    if(notes.value.length > 0) {
+        const confirmation = window.confirm("Are you sure you want to delete the notes?")
+        if(confirmation){
+            clearNotes()
+        }
+    } else{
+        const confirmation = window.confirm("There are no notes to delete")
+    }
+})
+
+//Clear only the checked items
+
+clearCheckedBtn.addEventListener("click", () => {
+    const checkboxes = document.getElementsByClassName('form-check-input')
+    const checkboxesArray = Array.from(checkboxes)
+    
+    for (const checkbox of checkboxesArray) {
+        if (checkbox.checked){
+            const listItem= checkbox.closest('li')
+            if(listItem){
+                listItem.remove()
+            }
+        }
+    }
 })
